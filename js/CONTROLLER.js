ae669ff8747026fafe48ab9ac243ffccf00fc98c
sfulleruwyo <sfu234@uky.edu>
2020-09-02 10:23:56 -0600

    Added Irrigation data
    Added Irrigation data to layers

---
+++ js/CONTROLLER.js
@@ -0,0 +1,131 @@
+let controller = {
+	"type" : "FeatureCollection",
+	"name" : "Controllers",
+	"features" : [
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8078475985, 39.506391958, 1816.037109375 ]
+			},
+			"properties" : {
+				"Controller" : "8",
+				"ofZones" : "13",
+				"Image1" : "images/insphere/f15c02eb-f88c-4511-abcb-cfe0f124e52e.jpg"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8073640068, 39.5014418209, 1824.5346679688 ]
+			},
+			"properties" : {
+				"Notes" : "Node 2",
+				"Image1" : "images/insphere/4003951d-b749-4382-a91c-4c7a2f04ebeb.jpg"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8073691816, 39.5014357671, 1823.8896484375 ]
+			},
+			"properties" : {
+				"Notes" : "Node 1",
+				"Image1" : "images/insphere/725adeb9-d5dd-4437-a448-a158aa14d47a.jpg"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8069150599, 39.5023834401, 1824.1945800781 ]
+			},
+			"properties" : {
+				"Controller" : "1",
+				"ofZones" : "2",
+				"Notes" : "Mc8",
+				"Image1" : "images/insphere/374655a0-1264-4bd5-a59c-4248bb85de50.jpg"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.80774215, 39.50536792, 1805.4836425781 ]
+			},
+			"properties" : {
+				"Controller" : "2",
+				"Brand" : "Hunter",
+				"ofZones" : "21",
+				"RainSensor" : "None",
+				"Condition" : "Good",
+				"Notes" : "New I core",
+				"Image1" : "images/insphere/53111209-521f-4a28-8eeb-f696dd42faed.jpg"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8080579674, 39.5038421405, 1821.2175292969 ]
+			},
+			"properties" : {
+				"Controller" : "7",
+				"ofZones" : "11",
+				"Notes" : "Mc12+",
+				"Image1" : "images/insphere/169a5a3a-ec54-4fb7-bd71-3e20e66ac8f7.jpg",
+				"Autofield1" : "2020-07-22T12:14:36.660666Z"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8068777, 39.50370123, 1821.7392578125 ]
+			},
+			"properties" : {
+				"Controller" : "6.2",
+				"Brand" : "Hunter",
+				"ofZones" : "19",
+				"RainSensor" : "Hunter",
+				"Condition" : "Good",
+				"Image1" : "images/insphere/0f58cde2-9342-4e58-b28e-0e1c3b2d627c.jpg"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.8071831768, 39.505107264, 1814.8508300781 ]
+			},
+			"properties" : {
+				"Controller" : "6",
+				"Brand" : "Hunter",
+				"ofZones" : "11",
+				"RainSensor" : "Hunter",
+				"Condition" : "Good",
+				"Notes" : "Pro C",
+				"Image1" : "images/insphere/d686107b-2c13-439a-a524-93f2079bd1e7.jpg",
+				"Autofield1" : "2020-07-22T15:51:04.274398Z"
+			}
+		},
+		{
+			"type" : "Feature",
+			"geometry" : {
+				"type" : "Point",
+				"coordinates" : [ -104.805620977, 39.50418631, 1827.0627441406 ]
+			},
+			"properties" : {
+				"Controller" : "4",
+				"ofZones" : "13",
+				"RainSensor" : "None",
+				"Condition" : "Needs Replaced",
+				"Notes" : "MC18plus",
+				"Image1" : "images/insphere/6ca68071-9f41-454b-ad04-df264d04ea4b.jpg",
+				"Autofield1" : "2020-07-22T15:07:08.084054Z"
+			}
+		}
+	]
+}
